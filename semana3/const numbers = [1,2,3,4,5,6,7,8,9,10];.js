b25665403ad507211159755815b00e1f7bd130a0
josuadmin <alteam18@hotmail.com>
2023-07-16 17:09:53 -0500

    clases de p_w

---
+++ semana3/const numbers = [1,2,3,4,5,6,7,8,9,10];.js
@@ -0,0 +1,15 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const pares = [];
+const impares = [];
+for (let i = 0; i < numbers.length; i++) {
+     if (numbers[i] % 2 === 0) {
+         pares.push(numbers[i]);
+     }else{
+         impares.push(numbers[i]);
+     }        
+}
+console.log(pares);
+console.log(impares);
+
+
+
